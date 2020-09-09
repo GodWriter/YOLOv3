49f9c137c0e1aad0b4e5beebf4f61a2e1e4b37bc
GodWriter <18917565247@163.com>
2020-09-09 13:42:09 +0800

    commit config

---
+++ train.py
@@ -0,0 +1,13 @@
+import os
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=100, help="number of epochs")
+    parser.add_argument("--batch_size", type=int, default=8, help="size of each image batch")
+    parser.add_argument("--gradient_accumulations", type=int, default=2, help="number of gradient accums before step")
+    parser.add_argument("--model_def", type=str, default="config/yolov3.cfg", help="path to model definition file")
+
+    opt = parser.parse_args()
+    print(opt)
---
+++ utils/parse_config.py
@@ -0,0 +1,28 @@
+def parse_model_config(path):
+    # 打开yolov3.config
+    file = open(path, 'r')
+
+    # 读取每一行，去除#，空格等字符
+    lines = file.read().split('\n')
+    lines = [x for x in lines if x and not x.startswith('#')]
+    lines = [x.rstrip().lstrip() for x in lines]
+
+    # 定义module_defs用于保存网络结构，由词典构建
+    module_defs = []
+    for line in lines:
+        # 若是以[为首，说明网络某个子结构初始
+        if line.startswith('['):
+            # 每遇到一个[，就定一个词典加入module_defs
+            module_defs.append({})
+            module_defs[-1]['type'] = line[1:-1].rstrip()
+
+            # 由于某些网络子结构不包括BN，为保证后续网络构建统一，故BN都初始化为0
+            if module_defs[-1]['type'] == 'convolutional':
+                module_defs[-1]['batch_normalize'] = 0
+        else:
+            # 将每行操作由=划分，并去除空格后加入module_defs中
+            key, value = line.split('=')
+            value = value.strip()
+            module_defs[-1][key.rstrip()] = value.strip()
+
+    return module_defs
