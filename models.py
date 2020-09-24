798658841dd3f6c9450a577e6749abb7a6e2af95
GodWriter <18917565247@163.com>
2020-09-24 14:43:38 +0800

    commit.

--- models.py
+++ models.py
@@ -241,4 +241,33 @@ class Darknet(nn.Module):
         self.module_def = parse_model_config(config_path)
         self.hyperparams, self.module_list = create_modules(self.module_def)
 
-        pass
+        # yolo_layers单独拿出来，对象中包含metrics得就是yolo_layers
+        self.yolo_layers = [layer[0] for layer in self.module_list if hasattr(layer[0], "metrics")]
+
+        self.img_size = img_size
+        self.seen = 0
+        self.header_info = np.array([0, 0, 0, self.seen, 0], dtype=np.int32)
+
+    def forward(self, x, targets=None):
+        img_dim = x.shape[2]
+
+        loss = 0
+        layer_outputs, yolo_outputs = [], []
+
+        for i, (module_def, module) in enumerate(zip(self.module_def, self.module_list)):
+            if module_def["type"] in ["convolutional", "upsample", "maxpool"]:
+                x = module(x)
+            elif module_def["type"] == "route":
+                x = torch.cat([layer_outputs[int(layer_i)] for layer_i in module_def["layers"].split(',')], 1)
+            elif module_def["type"] == "shortcut":
+                layer_i = int(module_def["from"])
+                x = layer_outputs[-1] + layer_outputs[layer_i]
+            elif module_def["type"] == "yolo":
+                x, layer_loss = module[0](x, targets, img_dim)
+                loss += layer_loss
+                yolo_outputs.append(x)
+            layer_outputs.append(x)
+
+        yolo_outputs = to_cpu(torch.cat(yolo_outputs, 1))
+        return yolo_outputs if targets is None else (loss, yolo_outputs)
+
